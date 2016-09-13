54c7f6c7e8135099763e16b18f35ca641ccee2c5
Vikram Adithya Ganesh <vikram.adithya@outlook.com>
2016-09-13 19:22:10 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,19 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    $('.slider').slider({interval: 5000});
+});
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
