b6df46722b771cee2ad6a1aaeb593cc616646de9
Vikram Adithya <callvikram22@gmail.com>
2016-10-18 22:33:42 +0530

    trying phpmailer

--- php/t.php
+++ php/t.php
@@ -1,21 +1,43 @@
-
 <?php
-// the message
-if(isset($_REQUEST['email'])){
-$msg = "Name:".$_REQUEST['name']."<br> Email:".$_REQUEST['email']."<br> Phone No:".$_REQUEST['phone']."<br> Message:".$_REQUEST['message']."<br>";
 
+require 'php/PHPMailerAutoload.php';
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,150);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-// send email
-mail("callvikram22@gmail.com","Form",$msg);
+$mail = new PHPMailer;
 
-echo "<p>Sent!!</p>";
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+//$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'callvikram22@gmail.com';                 // SMTP username
+$mail->Password = 'gansharvikvar';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom($email, $name);
+//$mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+// $mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Form';
+$mail->Body    = "Name:".$name." email:".$email." phone no:".$phone." message:".$message." ";
+//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
 }
-else{
-    echo "error";
-}
-?>
-    
   
