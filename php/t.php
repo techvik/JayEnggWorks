5b27801c4332b0b245a09522cb4e28f246b610ac
Vikram Adithya <callvikram22@gmail.com>
2016-10-18 21:43:10 +0530

    ds

--- php/t.php
+++ php/t.php
@@ -1,7 +1,8 @@
 
 <?php
 // the message
-$msg = "Name:".$_POST['name']."<br> Email:".$_POST['email']."<br> Phone No:".$_POST['phone']."<br> Message:".$_POST['message']."<br>";
+if(isset($_REQUEST['email'])){
+$msg = "Name:".$_REQUEST['name']."<br> Email:".$_REQUEST['email']."<br> Phone No:".$_REQUEST['phone']."<br> Message:".$_REQUEST['message']."<br>";
 
 
 // use wordwrap() if lines are longer than 70 characters
@@ -11,6 +12,10 @@ $msg = wordwrap($msg,150);
 mail("callvikram22@gmail.com","Form",$msg);
 
 echo "<p>Sent!!</p>";
+}
+else{
+    echo "error";
+}
 ?>
     
   
